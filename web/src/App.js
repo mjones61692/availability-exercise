c0975a0266fb9093cf6b57f728dc87fe857f5cca
Matthew Jones <mjones61692@gmail.com>
2019-04-30 15:15:07 -0400

    updated Availabilities component to use APIs response data

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import BookTime from './BookTime.js';
 import Availabilities from './Availabilities.js';
 import Bookings from './Bookings.js';
@@ -7,26 +8,42 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      today: null
+      today: null,
+      availabilityIds: [],
+      availabilityTimes: []
     };
-    this.fetchToday();
   }
 
-  async fetchToday() {
-    try {
-      const res = await fetch("http://localhost:4433/today");
-      const json = await res.json();
-      this.setState({today: json.today});
-    } catch (e) {
-      console.error("Failed to fetch 'today' data", e);
-    }
+  componentDidMount() {
+    let todayPromise = axios.get("http://localhost:4433/today")
+      .catch((err) => {
+        console.error("Failed to fetch 'today' data", err);
+      });
+
+    let availablilitiesPromise = axios.get("http://localhost:4433/availability")
+      .catch((err) => {
+        console.error("Failed to fetch 'availabilities' data", err);
+      });
+
+    Promise.all([todayPromise, availablilitiesPromise])
+      .then((results) => {
+        console.log(results);
+        this.setState({
+          today: results[0].data.today,
+          availabilityIds: results[1].data.ids,
+          availabilityTimes: results[1].data.times
+        })
+      })
+      .catch((err) => {
+        console.error("Promises failed to resolve", err);
+      });
   }
 
   render() {
     return (
       <div className="App container">
         <BookTime today={this.state.today} />
-        <Availabilities />
+        <Availabilities ids={this.state.availabilityIds} times={this.state.availabilityTimes}/>
         <Bookings />
       </div>
     );
--- web/src/Availabilities.js
+++ web/src/Availabilities.js
@@ -12,36 +12,24 @@ function Availabilities(props) {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>35545</td>
-              <td>
-                <ul className="list-unstyled">
-                  <li>
-                    <time dateTime="2019-04-04T13:00:00-04:00" className="book-time">4/4/2019 1:00 pm</time>
-                    <button className="book btn-small btn-primary">Book</button>
-                  </li>
-                  <li>
-                    <time dateTime="2019-04-05T10:00:00-04:00" className="book-time">4/5/2019 10:00 am</time>
-                    <button className="book btn-small btn-primary">Book</button>
-                  </li>
-                </ul>
-              </td>
-            </tr>
-            <tr>
-              <td>36232</td>
-              <td>
-                <ul className="list-unstyled">
-                  <li>
-                    <time dateTime="2019-04-02T13:00:00-04:00" className="book-time">4/2/2019 1:00 pm</time>
-                    <button className="book btn-small btn-primary">Book</button>
-                  </li>
-                  <li>
-                    <time dateTime="2019-04-03T11:00:00-04:00" className="book-time">4/3/2019 11:00 am</time>
-                    <button className="book btn-small btn-primary">Book</button>
-                  </li>
-                </ul>
-              </td>
-            </tr>
+            {props.ids.map((id, index) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>
+                  <ul className="list-unstyled">
+                  {props.times[index].map((time, index) => {
+                    let date = new Date(time);
+                    console.log(date);
+                    return (
+                      <li key={index}>
+                        <time dateTime={time} className="book-time">{date.toLocaleDateString()} {date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</time>
+                        <button className="book btn-small btn-primary">Book</button>
+                      </li>
+                  )})}
+                  </ul>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
     </React.Fragment>
